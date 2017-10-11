0a9163166bf167f00e910c548b2e45b6a4c9b975
hungchicheng <tommy11777@livemail.tw>
2017-10-11 19:30:56 +0800

    coding style
    coding style

--- C++/Observer.cpp
+++ C++/Observer.cpp
@@ -9,23 +9,23 @@ public:
 
 class Subject{
 protected:
-    std::set<Observer*> m_observerList;
+    std::set< Observer* > m_observerList;
 public:
     void attach( Observer *o ){ m_observerList.insert( o ); };
     void detach( Observer *o ){ m_observerList.erase( o ); };
     virtual void notify () = 0;
 };
 
-class  Subject1: public Subject{
+class  Subject1:public Subject{
 private:
     int m_state;
 public:
     void notify (){
-        for (auto &o : m_observerList){
+        for ( auto &o : m_observerList ){
             o->update(m_state);
         }
     };
-    void setState(int s){
+    void setState( int s ){
         m_state = s;
         notify();
     }
@@ -47,7 +47,7 @@ class Observer2:public Observer{
     int m_state;
 public:
     Observer2( string name ):m_name( name ){}
-    void update(int p){ m_state = p; } // override
+    void update( int p ){ m_state = p; } // override
     string getName(){ return m_name; } 
     int getState(){ return m_state; }
 };
@@ -55,17 +55,17 @@ public:
 int main(int argc, char* argv[])
 {
     Subject1 product;
-    Observer1 shop1("shop1--");
-    Observer2 shop2("shop2--");
+    Observer1 shop1( "shop1--" );
+    Observer2 shop2( "shop2--" );
     
-    product.attach(&shop1);
-    product.attach(&shop2);
-    product.setState(12);
+    product.attach( &shop1 );
+    product.attach( &shop2 );
+    product.setState( 12 );
     cout<< shop1.getName() << shop1.getState() <<endl;
     cout<< shop2.getName() << shop2.getState() <<endl;
     
-    product.detach(&shop2);
-    product.setState(11);
+    product.detach( &shop2 );
+    product.setState( 11 );
     cout<< shop1.getName() << shop1.getState() <<endl;
     cout<< shop2.getName() << shop2.getState() <<endl;
     
